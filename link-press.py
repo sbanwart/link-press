66940171f96250cae796b8208da25714ceb35212
Scott Banwart <sbanwart@rogue-technology.com>
2012-09-11 22:17:42 -0400

    Added code to generate database tables during first run of the program.

--- link-press.py
+++ link-press.py
@@ -16,12 +16,29 @@ if not os.path.isdir(data_path):
 
 db_path = os.path.join(data_path, dbname)
 
-dbConn = sqlite3.connect(db_path)
-cur = dbConn.cursor()
-cur.execute("SELECT SQLITE_VERSION()")
+try:
+    dbConn = sqlite3.connect(db_path)
+    cur = dbConn.cursor()
 
-data = cur.fetchone()
+    cur.execute("SELECT name FROM sqlite_master WHERE type = 'table' AND name = 'links'")
+    data = cur.fetchone()
 
-print "SQLite version: %s" % data
+    if not data:
+        print "Building database tables"
+        cur.execute("CREATE TABLE links(id INTEGER PRIMARY KEY, url TEXT, title TEXT, attribute_link_id INTEGER)")
+        cur.execute("CREATE TABLE tags(id INTEGER PRIMARY KEY, name TEXT)")
+        cur.execute("CREATE TABLE attribute_links(id INTEGER PRIMARY KEY, url TEXT, name TEXT, title TEXT)")
+        dbConn.commit()
+    else:
+        print "Database ready"
 
-dbConn.close()
+except sqlite3.Error, err:
+    if dbConn:
+        dbConn.rollback()
+
+    print "Error during database check: %s" % err.args[0]
+    sys.exit(1)
+
+finally:
+    if dbConn:
+        dbConn.close()
