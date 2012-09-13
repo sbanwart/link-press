162d0f03b4f3eb53fc3b62b696faf8cc0a41d4bd
Scott Banwart <sbanwart@rogue-technology.com>
2012-09-12 21:40:05 -0400

    Added command line argument parsing.

--- link-press.py
+++ link-press.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sqlite3
 import os
 import os.path
+import sqlite3
+import argparse
 
 user_home = os.path.expanduser("~")
 data_dir = ".link-press"
@@ -24,14 +25,21 @@ try:
     data = cur.fetchone()
 
     if not data:
-        print "Building database tables"
         cur.execute("CREATE TABLE links(id INTEGER PRIMARY KEY, url TEXT, title TEXT, attribute_link_id INTEGER)")
         cur.execute("CREATE TABLE tags(id INTEGER PRIMARY KEY, name TEXT)")
         cur.execute("CREATE TABLE attribute_links(id INTEGER PRIMARY KEY, url TEXT, name TEXT, title TEXT)")
         dbConn.commit()
-    else:
-        print "Database ready"
+    
+    parser = argparse.ArgumentParser(description = 'Create a link blog entry and post it to WordPress')
+    parser.add_argument('url')
+    parser.add_argument('name')
+    parser.add_argument('category')
+    parser.add_argument('tags')
+    parser.add_argument('--attribute_url', '-u')
+    parser.add_argument('--attribute_title', '-t')
+    parser.add_argument('--attribute_name', '-n')
 
+    args = parser.parse_args()
 except sqlite3.Error, err:
     if dbConn:
         dbConn.rollback()
